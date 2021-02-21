197cc23696404a2326b0531d8eeb454ae927f196
KangWon LEE <kangwon.lee@kpu.ac.kr>
2021-02-21 16:00:15 +0900

    added pytest file

--- test_string.py
+++ test_string.py
@@ -1,37 +1,47 @@
-import main
 import random
+import urllib.request as ur
+from typing import List
+
 import pytest
 
+import main
 
 random.seed()
 
 
 @pytest.fixture
-def sample_string():
-    return random.randint(-100, 100), random.randint(-100, 100)
+def url():
+    return 'https://www.mit.edu/~ecprice/wordlist.10000'
 
 
 @pytest.fixture
-def str_a_b():
-    return chr(random.randint(ord('!'), ord('~'))), chr(random.randint(ord('!'), ord('~')))
+def words(url, scope='session') -> List[bytes]:
+    # https://docs/python.org/3/library/urllib.request.html
+    with ur.urlopen(url) as req:
+        txt_lines = req.readlines()
+    return txt_lines
 
 
-def test_a_plus_twice_b_int_type(int_a_b):
-    a, b = int_a_b
+@pytest.fixture
+def sample_string(words:List[str]) -> str:
+    # https://docs/python.org/3/library/random.html
+    min_words = 5
+    assert len(words) >= min_words
+    n = random.randint(min_words, min(10, len(words)))
+    word_list = list(map(lambda w:w.decode().strip(), random.choices(words, k=n)))
+    random.shuffle(word_list)
+    return ' '.join(word_list)
 
-    result = main.a_plus_twice_b(a, b)
 
-    assert isinstance(result, int), "반환된 결과값의 자료형을 확인하시오 Check the type of the return value"
-    assert not ((result - a) // 2 - b), "함수의 반환값이 예상결과와 같은지 확인하시오 Verify the return value"
+def test_first_word(sample_string:str):
+    result = main.first_word(sample_string)
+    assert sample_string.startswith(result)
 
 
-def test_a_plus_twice_b_str_type(str_a_b):
-    a, b = str_a_b
+def test_last_word(sample_string:str):
+    result = main.last_word(sample_string)
+    assert sample_string.endswith(result)
 
-    result = main.a_plus_twice_b(a, b)
 
-    assert isinstance(result, str), "반환된 결과값의 자료형을 확인하시오 Check the type of the return value"
-    assert not ((len(result) - len(a))//2 - len(b)), "함수의 반환값이 예상결과와 같은지 확인하시오 Verify the return value"
-    assert a == result[0], "함수의 반환값이 예상결과와 같은지 확인하시오 Verify the return value"
-    assert b == result[-1], "함수의 반환값이 예상결과와 같은지 확인하시오 Verify the return value"
-    assert b == result[-2], "함수의 반환값이 예상결과와 같은지 확인하시오 Verify the return value"
+if "__main__" == __name__:
+    pytest.main()
