c8ff3bdf606d8325e72766745135d8974ee09adc
KangWon LEE <kangwon.lee@kpu.ac.kr>
2021-02-21 15:27:38 +0900

    updated main()

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ def last_word(a:str):
 
 
 def main():
-    print("a_plus_twice_b(2, 1) =", a_plus_twice_b(2, 1))
-    print("a_plus_twice_b('a', 'b') =", a_plus_twice_b('a', 'b'))
+    print('''first_word("파이썬 프로그래밍 언어") =''', first_word("파이썬 프로그래밍 언어"))
+    print('''last_word("Python Programming Language") =''', last_word("Python Programming Language"))
 
 
 if "__main__" == __name__:
